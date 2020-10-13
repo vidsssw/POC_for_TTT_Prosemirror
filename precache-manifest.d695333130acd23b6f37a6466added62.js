2cfaa617f9dc5d9ebdf98cc40badf5134d65b1db
Vidisha Wagle <wagle.vidisha@sitpune.edu.in>
2020-10-14 01:27:30 +0530

    Updates

---
+++ precache-manifest.d695333130acd23b6f37a6466added62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c227aec75730e5e3556ae5773bb5121",
+    "url": "/prosemirror_poc/index.html"
+  },
+  {
+    "revision": "218aa91b64164197f8dc",
+    "url": "/prosemirror_poc/static/css/main.6628032d.chunk.css"
+  },
+  {
+    "revision": "9c257fae910192c48589",
+    "url": "/prosemirror_poc/static/js/2.5e15a2ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prosemirror_poc/static/js/2.5e15a2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767dd69780220e27568e",
+    "url": "/prosemirror_poc/static/js/3.ade755ee.chunk.js"
+  },
+  {
+    "revision": "817286109b45ab4e5f50",
+    "url": "/prosemirror_poc/static/js/4.e54ef3d2.chunk.js"
+  },
+  {
+    "revision": "218aa91b64164197f8dc",
+    "url": "/prosemirror_poc/static/js/main.04021765.chunk.js"
+  },
+  {
+    "revision": "48f3d33ff0df0b1e6c56",
+    "url": "/prosemirror_poc/static/js/runtime-main.3084e925.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prosemirror_poc/static/media/logo.5d5d9eef.svg"
+  }
+]);
